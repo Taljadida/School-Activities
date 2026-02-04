8e4ae87faa8212177eb59f1669497a7583c47637
Taljadida <eltal_eljadida@hotmail.com>
2026-02-04 18:44:30 +0200

    Add initial files to data.js

---
+++ data.js
@@ -0,0 +1,18 @@
+const data = {
+  activities: [
+    "activities/activity1.jpg"
+  ],
+  events: [
+    "events/event1.jpg"
+  ],
+  awards: [
+    "awards/award1.jpg"
+  ],
+  photos: [
+    "photos/photo1.jpg",
+    "photos/photo2.jpg"
+  ],
+  videos: [
+    "videos/video1.mp4"
+  ]
+};
