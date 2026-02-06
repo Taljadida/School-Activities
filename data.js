3dc8c0f8c0b0e0d824b8b71213b8581cad392807
Taljadida <eltal_eljadida@hotmail.com>
2026-02-06 22:51:39 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,23 +1,23 @@
 const data = {
-  Photos: [
-    "Photos/photo1.jfif",
+  photos: [
+    "photos/photo1.jfif",
     
   ],
-  Videos: [
-    "Videos/Video6.mp4",
-    "Videos/Video2.mp4",
+  videos: [
+    "videos/Video6.mp4",
+    "videos/Video2.mp4",
     
   ],
-  Activities: [
-    "Activities/activity1.jpg"
+  activities: [
+    "activities/activity1.jpg"
   ],
-  Events: [
-    "Events/video1.mp4",
-    "Events/video5.mp4",
-    "Events/video3.mp4",
+  events: [
+    "events/video1.mp4",
+    "events/video5.mp4",
+    "events/video3.mp4",
   ],
-  Awards: [
-    "Awards/photo2.jpeg",
-    "Awards/photo3.jpeg"
+  awards: [
+    "awards/photo2.jpeg",
+    "awards/photo3.jpeg"
   ]
 };
