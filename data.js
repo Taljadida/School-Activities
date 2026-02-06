59552904aa3d7ce539ac62f20434a3ef6a38ce4d
Taljadida <eltal_eljadida@hotmail.com>
2026-02-06 20:13:51 +0200

    Update data.js

--- data.js
+++ data.js
@@ -12,7 +12,8 @@ const data = {
     "activities/activity1.jpg"
   ],
   events: [
-    "events/video1.mp4"
+    "events/video1.mp4",
+    "events/video5.mp4"
   ],
   awards: [
     "awards/photo2.jpeg",
